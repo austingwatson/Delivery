93b17a90afcd6ca2df889b7d4fbe4570e258661f
Austin Watson <austingwatson@hotmail.com>
2023-04-30 19:53:28 -0500

    added goblin behavior

--- scripts/Entities.gd
+++ scripts/Entities.gd
@@ -2,8 +2,23 @@ extends Node2D
 
 const goblin_scene = preload("res://scenes/entity/Goblin.tscn")
 
-onready var y_sort = $YSort
 var cart
+
+onready var y_sort = $YSort
+
+
+func _physics_process(delta):
+	var children = y_sort.get_children()
+	
+	children.sort_custom(EntitiesSorter, "sort_entities")
+	var found_cart = false
+	for child in children:
+		child.z_index = 2
+		if found_cart:
+			child.z_index = 25
+		elif child == cart:
+			found_cart = true
+			child.z_index = 10
 
 
 func restart():
@@ -14,7 +29,7 @@ func restart():
 	y_sort.add_child(cart)
 	cart.position = Vector2(0, 0)
 	
-	print("restart")
+	add_random_goblin(100)
 
 
 func add_to_ysort(entity):
@@ -31,3 +46,12 @@ func add_random_goblin(y_spawn):
 		goblin.position.y = y_spawn
 	else:
 		goblin.position.y = -y_spawn
+
+
+class EntitiesSorter:
+	static func sort_entities(a, b):
+		if a.position.y < b.position.y:
+			return true
+		else:
+			return false
+			
--- scripts/entity/Goblin.gd
+++ scripts/entity/Goblin.gd
@@ -1,16 +1,107 @@
 extends Area2D
 
+enum State {
+	IDLE,
+	MOVE,
+	TAUNT,
+	HURT,
+	ACTION,
+	DEATH,
+}
+
 export var speed = 10.0
 export var health = 1
 
+var state = State.IDLE
+
+onready var animated_sprite = $AnimatedSprite
+onready var remove_timer = $RemoveTimer
+onready var idle_timer = $IdleTimer
+onready var attack_range_shape = $AttackRange/CollisionShape2D
+
+
+func _ready():
+	animated_sprite.play("idle")
+	idle_timer.start()
+
 
 func _physics_process(delta):
-	var velocity = position.direction_to(Entities.cart.position) * speed * delta
-	position += velocity
+	if state == State.MOVE:
+		move(delta)
+	elif state == State.ACTION:
+		if global_position.distance_to(Entities.cart.global_position) > 25:
+			move(delta)
+
+
+func move(delta):
+	position += position.direction_to(Entities.cart.position) * speed * delta
+			
+	if global_position.x > Entities.cart.global_position.x:
+		animated_sprite.flip_h = false
+	elif global_position.x < Entities.cart.global_position.x:
+		animated_sprite.flip_h = true
+			
+
+
+func action_done():
+	print("goblin did action")
 
 
 func damage(amount):
 	health -= amount
-	print(health)
 	if health <= 0:
-		queue_free()
+		animated_sprite.play("death")
+		state = State.DEATH
+	else:
+		animated_sprite.play("hurt")
+		state = State.HURT
+
+
+func _on_AnimatedSprite_animation_finished():
+	match state:
+		State.DEATH:
+			animated_sprite.stop()
+			animated_sprite.frame = 6
+			remove_timer.start()
+		State.HURT:
+			animated_sprite.play("idle")
+			state = State.IDLE
+			idle_timer.start()
+		State.TAUNT:
+			animated_sprite.play("move")
+			state = State.MOVE
+			attack_range_shape.set_deferred("disabled", false)
+		State.ACTION:
+			action_done()
+			animated_sprite.play("taunt")
+			state = State.TAUNT
+		
+			if global_position.x > Entities.cart.global_position.x:
+				animated_sprite.flip_h = false
+			elif global_position.x < Entities.cart.global_position.x:
+				animated_sprite.flip_h = true
+
+
+func _on_RemoveTimer_timeout():
+	queue_free()
+
+
+func _on_IdleTimer_timeout():
+	var rng = randi() % 2
+	if rng == 0:
+		animated_sprite.play("move")
+		state = State.MOVE
+	else:
+		animated_sprite.play("taunt")
+		state = State.TAUNT
+		
+		if global_position.x > Entities.cart.global_position.x:
+			animated_sprite.flip_h = false
+		elif global_position.x < Entities.cart.global_position.x:
+			animated_sprite.flip_h = true
+
+
+func _on_AttackRange_area_entered(area):
+	animated_sprite.play("action")
+	state = State.ACTION
+	attack_range_shape.set_deferred("disabled", true)
